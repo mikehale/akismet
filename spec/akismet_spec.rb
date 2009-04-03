80eabfab5392eabd19944f1eb6d5bc68076fb00a
Michael Hale <mikehale@gmail.com>
2009-04-03 12:20:17 -0400

    added verify_key

--- spec/akismet_spec.rb
+++ spec/akismet_spec.rb
@@ -1,7 +1,37 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
-describe "Akismet" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe "Akismet with valid key and url" do
+  before do
+    @io = HttpResponse.ok({}, "valid")
+    mock(TCPSocket).open('rest.akismet.com', 80) { @io }
+    @akismet = Akismet.new('thekey', 'theblog')
   end
+
+  it "should send the proper user agent" do
+    @akismet.verify_key # only needed to make the request
+    @io.written.should include(Akismet::USER_AGENT)
+  end
+
+  it "should verify the key" do
+    @akismet.verify_key.should == true
+  end
+
+  it "should check a comment"
+  it "should submit spam"
+  it "should submit ham"
+end
+
+describe "Akismet with invalid key and url" do
+  before do
+    @io = HttpResponse.ok({"x-akismet-debug-help"=>["debug help"]}, "invalid")
+    mock(TCPSocket).open('rest.akismet.com', 80) { @io }
+    @akismet = Akismet.new('thekey', 'theblog')
+  end
+
+  it "should not verify the key" do
+    lambda {@akismet.verify_key}.should raise_error(Akismet::VerifyException)
+  end
+
+  # don't know how to test this yet
+  it "should raise an exception with the content of the x-akismet-debug-help header"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,53 @@
 require 'spec'
+require 'rr'
+require 'ruby-debug'
 
-$LOAD_PATH.unshift(File.dirname(__FILE__))
+class MethodSpy
+  def initialize(delegate, &block)
+    @delegate = delegate
+    @filter = block
+  end
+
+  def method_missing(symbol, *args, &block)
+    result = @delegate.send(symbol, *args, &block)
+    p [symbol, args, result, block]
+    result
+  end
+end
+
+class HttpResponse
+  def self.ok(headers, body)
+    response = [%(HTTP/1.1 200 OK)]
+    headers.each{|k,vs|
+      if vs.is_a?(Array)
+        response << vs.map{|v| "#{k.to_s}: #{v.to_s}" }
+      else
+        response << "#{k.to_s}: #{vs.to_s}"
+      end
+    }
+    response << ''
+    response << %(#{body})
+    response = response.join("\r\n")
+
+    io = StringIO.new(response)
+    def io.written
+      @written
+    end
+
+    def io.write(content)
+      @written = '' unless @written
+      @written << content
+      0
+    end
+
+    io
+  end
+end
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'akismet'
 
 Spec::Runner.configure do |config|
-  
+  config.mock_with :rr
 end
