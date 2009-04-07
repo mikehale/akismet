7e992b532b30aaf4632d203ea4d958557a6b4ac0
Michael Hale <mikehale@gmail.com>
2009-04-07 04:32:07 -0400

    test the request and response

--- spec/akismet_spec.rb
+++ spec/akismet_spec.rb
@@ -9,8 +9,11 @@ describe "Akismet" do
   
   it "should verify the key" do
     map Rack::URLMap.new("http://rest.akismet.com/" => lambda { |env| [200, {}, ["valid"]]})
+    
     @akismet.verify_key.should == true
     request.post?.should == true
+    request.env['HTTP_USER_AGENT'].should == "Akismet-rb/1.0 | Akismet/1.11"
+    request.body.should include('http://example.com')
     response.status.should == 200
   end
   
